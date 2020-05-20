aeb82cbb8a23477c6eb3a91466f6c786d145067b
jamaljfrank <jamaljfrank@gmail.com>
2020-05-19 20:37:00 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+function writeCards(names, occasion) {
+  let i = 0;
+  while (i < names.length) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    i++;
+  }
+
+  return names;
+}
+
+writeCards(names, occasion);
+
+
+
+
+
